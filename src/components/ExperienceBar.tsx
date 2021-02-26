39425bd44e935147f4d85fa72452301e2451a11c
Gustavo <gustavo.ferreira0821@gmail.com>
2021-02-25 23:34:46 -0300

    V1.0 - Done, the migration to NextJs

--- src/components/ExperienceBar.tsx
+++ src/components/ExperienceBar.tsx
@@ -1,7 +1,7 @@
 export function ExperienceBar() {
     return (
         <header className='experience-bar'>
-            <span>:0 xp</span>
+            <span>0 xp</span>
             <div>
                 <div style={{ width: '50%' }}>
                     <span className="current-experience" style={{left:'50%'}}>300xp</span>
