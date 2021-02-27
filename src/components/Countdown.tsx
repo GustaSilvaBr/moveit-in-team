3b5d2488f392dec7debf76d0cd0cd344c51cf54d
Gustavo <gustavo.ferreira0821@gmail.com>
2021-02-26 22:33:25 -0300

    V1.0 - Created, function to do the countdown with useEffect

--- src/components/Countdown.tsx
+++ src/components/Countdown.tsx
@@ -1,21 +1,48 @@
+import {useState, useEffect} from 'react';
 import styles from '../styles/components/Countdown.module.css';
 
 export function Countdown() {
+    const maxMinutes = 1;
+    const maxSecunds = 60;
+    const maxMinutesInSecunds = maxMinutes * maxSecunds;
+
+    const [time, setTime] = useState(maxMinutesInSecunds);
+    const [active, setActive] = useState(false);
+
+    const minutes = Math.floor(time/maxSecunds);
+
+    const secunds = time % maxSecunds;
+
+    const [minuteLeft, minuteRight] = String(minutes).padStart(2, '0').split('');
+
+    const [secundLeft, secundRight] = String(secunds).padStart(2, '0').split('');
+
+    function startCountdonw(){
+        setActive(true);
+    }
+
+    useEffect(()=>{
+        if(active && time > 0){
+            setTimeout(()=>{
+                setTime(time-1);
+            },1000)
+        }
+    },[active, time]);
+
     return (
         <div>
             <div className={styles.countdownContainer}>
                 <div>
-                    <span>2</span>
-                    <span>5</span>
-
+                    <span>{minuteLeft}</span>
+                    <span>{minuteRight}</span>
                 </div>
                 <span>:</span>
                 <div>
-                    <span>0</span>
-                    <span>0</span>
+                    <span>{secundLeft}</span>
+                    <span>{secundRight}</span>
                 </div>
             </div>
-            <button type="button" className={styles.countdownButton}>
+            <button type="button" className={styles.countdownButton} onClick={startCountdonw}>
                 Iniciar um ciclo
             </button>
         </div>
